ae1eddd7658b2279d3564facf36c3ff2c52bd3a8
Binjaaa <binya87@gmail.com>
2016-07-22 01:32:43 +0200

    Add DirectoryHelper

---
+++ src/HtmlLogger/DirectoryHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace HtmlLogger
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal static class DirectoryHelper
+    {
+    }
+}
