89ed139c1dcf076137173cd334ed2af85afff8d6
Binjaaa <binya87@gmail.com>
2016-07-22 01:08:10 +0200

    Add ScreenCapturer class.

---
+++ src/HtmlLogger/Contracts/IScreenCapturer.cs
@@ -0,0 +1,22 @@
+﻿
+namespace HtmlLogger.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IScreenCapturer
+    {
+        Image CaptureScreen();
+        void CaptureScreenToFile(string filename, ImageFormat format);
+
+        Image CaptureWindow(IntPtr handle);
+        void CaptureWindowToFile(IntPtr handle, string filename, ImageFormat format);
+
+        IntPtr GetActiveWindowHandle();
+    }
+}
