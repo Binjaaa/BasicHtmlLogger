c7f7cd9b7b88a86db08b11c8e9442a0bee6449d7
Binjaaa <binya87@gmail.com>
2016-07-25 01:26:12 +0200

    Add IoHelper to provide testable solution to IO-related operations.

---
+++ src/HtmlLogger/Contracts/IIoHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace HtmlLogger.Contracts
+{
+    using System;
+    using System.Drawing;
+
+    public interface IIoHelper
+    {
+        bool FileExists(Uri filePath);
+
+        void SavePictureTo(Image image, string path);
+    }
+}
---
+++ src/HtmlLogger/IOHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace HtmlLogger
+{
+    using Contracts;
+    using System;
+    using System.Drawing;
+    using System.IO;
+
+    internal class IoHelper : IIoHelper
+    {
+        public bool FileExists(Uri filePath)
+        {
+            return File.Exists(filePath.AbsolutePath);
+        }
+
+        public void SavePictureTo(Image image, string path)
+        {
+            //TODO: Format is: screenshots/2016-07-22_10-26-02.png
+        }
+    }
+}
