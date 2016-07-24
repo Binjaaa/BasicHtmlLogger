45457a2af84bfac015dbd0be85ae8f97eef59508
Binjaaa <binya87@gmail.com>
2016-07-25 01:28:39 +0200

    Add Guard class to encapsulate generic argument shield functionalities.

---
+++ src/HtmlLogger/Utils/Guard.cs
@@ -0,0 +1,38 @@
+﻿namespace HtmlLogger.Utils
+{
+    using System;
+
+    internal class Guard
+    {
+        private static Guard _against;
+
+        public static Guard Against
+        {
+            get
+            {
+                if (_against == null)
+                {
+                    _against = new Guard();
+                }
+
+                return _against;
+            }
+        }
+
+        public void Null(string name, object value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(name);
+            }
+        }
+
+        public void NullOrEmpty(string name, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException($"string value is null or empty: {name}");
+            }
+        }
+    }
+}
